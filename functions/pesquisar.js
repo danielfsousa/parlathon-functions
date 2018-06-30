e30d5b70b1bed973de0a3418f92598c6bcd4a5b5
Danilo Lemes <daniloleemes@hotmail.com>
2018-06-30 11:06:55 -0300

    Adiciona pesquisa

--- functions/pesquisar.js
+++ functions/pesquisar.js
@@ -1,19 +1,50 @@
 const querystring = require('querystring')
 const axios = require('axios')
-const { get } = require('lodash')
+const {get, random, take, partial} = require('lodash')
 
 module.exports = async (context) => {
 
     const {params} = context
+
+    const urlProposicoesDetalhesSenado = id => `http://legis.senado.leg.br/dadosabertos/materia/${id}`
+    const urlProposicoes = query => `https://dadosabertos.camara.leg.br/api/v2/proposicoes?${query}`
+    const urlAutores = id => `https://dadosabertos.camara.leg.br/api/v2/proposicoes/${id}/autores`
+
 
     const httpGetCamara = async (params) => {
         const res = await axios.get("https://dadosabertos.camara.leg.br/api/v2/proposicoes", {params});
         return get(res, 'data.dados', [])
     }
 
+    const httpGet = async (url, def) => {
+        if (!url) return {}
+        const res = await axios.get(url)
+        const { data = {} } = res
+        return data.dados || def
+    }
+
     const httpGetSenado = async (params) => {
-        const res = await axios.get("http://legis.senado.leg.br/dadosabertos/materia/pesquisa/lista", {params, headers: { 'Accept': "application/json" }});
+        const res = await axios.get("http://legis.senado.leg.br/dadosabertos/materia/pesquisa/lista", {
+            params,
+            headers: {'Accept': "application/json"}
+        });
         return get(res, 'data.PesquisaBasicaMateria.Materias.Materia', [])
+    }
+
+    const httpGetOneMateriaSenado = async (url) => {
+        const res = await axios.get(url)
+        return get(res, 'data.DetalheMateria.Materia', {})
+    }
+
+    const getAutorAleatorio = (proposicao) => {
+        const autores = get(proposicao, 'Autoria.Autor', [])
+        const autorAleatorio = autores[random(0, autores.length - 1)] || {}
+        const getProp = partial(get, autorAleatorio)
+        return {
+            id: getProp('IdentificacaoParlamentar.CodigoParlamentar'),
+            nome: getProp('NomeAutor'),
+            foto: getProp('IdentificacaoParlamentar.UrlFotoParlamentar')
+        }
     }
 
     const getHousesParams = (type, params) => {
@@ -42,36 +73,77 @@ module.exports = async (context) => {
         return p
     }
 
-    // {
-    //     id: proposicao.IdentificacaoMateria.CodigoMateria,
-    //     siglaTipo: proposicao.IdentificacaoMateria.SiglaSubtipoMateria,
-    //     dataApresentacao: new Date(proposicao.DadosBasicosMateria.DataApresentacao),
-    //     situacao: proposicao.SituacaoAtual.Autuacoes.Autuacao.Situacao.DescricaoSituacao,
-    //     numero: proposicao.IdentificacaoMateria.NumeroMateria,
-    //     ano: proposicao.IdentificacaoMateria.AnoMateria,
-    //     ementa: proposicao.DadosBasicosMateria.EmentaMateria,
-    //     autor: getAutorAleatorio(proposicao),
-    //     casa: CASA
-    //   }
-    // http://legis.senado.leg.br/dadosabertos/materia/pesquisa/lista?palavraChave=Econômico&tipoPalavraChave=T
-    // https://dadosabertos.camara.leg.br/api/v2/proposicoes?ano=&ordenarPor=ano&ordem=ASC&siglaTipo=PLC&numero
-    // return "Hello World!"
-
     const objetoCamara = httpGetCamara(getHousesParams(1, params))
-    const objetoSenado = httpGetSenado(getHousesParams(2, params))
 
-    // {
-    //     id: proposicao.IdentificacaoMateria.CodigoMateria,
-    //     siglaTipo: proposicao.IdentificacaoMateria.SiglaSubtipoMateria,
-    //     dataApresentacao: new Date(proposicao.DadosBasicosMateria.DataApresentacao),
-    //     situacao: proposicao.SituacaoAtual.Autuacoes.Autuacao.Situacao.DescricaoSituacao,
-    //     numero: proposicao.IdentificacaoMateria.NumeroMateria,
-    //     ano: proposicao.IdentificacaoMateria.AnoMateria,
-    //     ementa: proposicao.DadosBasicosMateria.EmentaMateria,
-    //     autor: getAutorAleatorio(proposicao),
-    //     casa: CASA
-    //   }
+    const proposicoesSenado = await httpGetSenado(getHousesParams(2, params))
+    const proposicoesDetalhadasSenado = await Promise.all(
+        proposicoesSenado
+            .map(p => p.IdentificacaoMateria.CodigoMateria)
+            .map(urlProposicoesDetalhesSenado)
+            .map(httpGetOneMateriaSenado)
+    )
 
-    return objetoSenado;
-    // return 123;
+    let propsSenado = proposicoesDetalhadasSenado.map((proposicao) => ({
+        id: proposicao.IdentificacaoMateria.CodigoMateria,
+        siglaTipo: proposicao.IdentificacaoMateria.SiglaSubtipoMateria,
+        dataApresentacao: new Date(proposicao.DadosBasicosMateria.DataApresentacao),
+        situacao: proposicao.SituacaoAtual.Autuacoes.Autuacao.Situacao.DescricaoSituacao,
+        numero: proposicao.IdentificacaoMateria.NumeroMateria,
+        ano: proposicao.IdentificacaoMateria.AnoMateria,
+        ementa: proposicao.DadosBasicosMateria.EmentaMateria,
+        autor: getAutorAleatorio(proposicao),
+        casa: {
+            id: 2,
+            nome: 'Senado Federal'
+        }
+    }));
+
+    /**
+     * Camara
+     */
+
+    const proposicoes = await httpGetCamara(getHousesParams(1, params))
+    const proposicoesUri = proposicoes.map(p => p.uri)
+    const proposicoesDetalhesPromises = proposicoesUri.map(p => httpGet(p, {}))
+    const proposicoesDetalhes = await Promise.all(proposicoesDetalhesPromises)
+
+    // Autores
+
+    const autores = await Promise.all(proposicoes.map(p => httpGet(urlAutores(p.id))))
+    const autoresAleatorios = autores.map(a => a[random(0, a.length - 1)] || {})
+
+    const autoresDetalhes = await Promise.all(autoresAleatorios.map(autor => {
+        return autor.uri ? httpGet(autor.uri) : {}
+    }))
+
+    const autoresComFotos = autoresAleatorios.map((autor, index) => {
+        const id = autor.uri && autor.uri.match(/deputados\/(.*)/)[1]
+        const detalhes = autoresDetalhes[index].ultimoStatus || {}
+        return {
+            id,
+            nome: autor.nome,
+            tipo: autor.tipo,
+            partido: detalhes.siglaPartido,
+            foto: detalhes.urlFoto
+        }
+    })
+
+    // Join
+
+    let propsCamara = proposicoes.map((proposicao, index) => ({
+        id: proposicao.id,
+        dataApresentacao: new Date(get(proposicoesDetalhes[index], 'dataApresentacao')),
+        siglaTipo: proposicao.siglaTipo,
+        numero: proposicao.numero,
+        situacao: get(proposicoesDetalhes[index], 'statusProposicao.descricaoSituacao'),
+        ano: proposicao.ano,
+        ementa: proposicao.ementa,
+        autor: autoresComFotos[index],
+        casa: {
+            id: 1,
+            nome: 'Câmara dos Deputados'
+        }
+    }))
+
+    return [...propsCamara, ...propsSenado]
 }
--- functions/projetos-de-lei-do-senado.js
+++ functions/projetos-de-lei-do-senado.js
@@ -1,63 +1,63 @@
 const axios = require('axios')
 const querystring = require('querystring')
-const { get, random, take, partial } = require('lodash')
+const {get, random, take, partial} = require('lodash')
 
 /**
-* Listar projetos de lei da Senado federal
-* @returns {array} Lista de projetos de lei do senado
-*/
+ * Listar projetos de lei da Senado federal
+ * @returns {array} Lista de projetos de lei do senado
+ */
 module.exports = async (context) => {
-  const CASA = {
-    id: 2,
-    nome: 'Senado Federal'
-  }
-
-  const urlProposicoes = query => `http://legis.senado.leg.br/dadosabertos/materia/atualizadas?${query}`
-  const urlProposicoesDetalhes = id => `http://legis.senado.leg.br/dadosabertos/materia/${id}`
-
-  const httpGetManyMaterias = async (url) => {
-    const res = await axios.get(url)
-    return get(res, 'data.ListaMateriasAtualizadas.Materias.Materia', [])
-  }
-
-  const httpGetOneMateria = async (url) => {
-    const res = await axios.get(url)
-    return get(res, 'data.DetalheMateria.Materia', {})
-  }
-
-  const getAutorAleatorio = (proposicao) => {
-    const autores = get(proposicao, 'Autoria.Autor', [])
-    const autorAleatorio = autores[random(0, autores.length - 1)] || {}
-    const getProp = partial(get, autorAleatorio)
-    return {
-      id: getProp('IdentificacaoParlamentar.CodigoParlamentar'),
-      nome: getProp('NomeAutor'),
-      foto: getProp('IdentificacaoParlamentar.UrlFotoParlamentar')
+    const CASA = {
+        id: 2,
+        nome: 'Senado Federal'
     }
-  }
 
-  const parametros = { ...context.params }
-  parametros.sigla = get(context, 'params.siglaTipo', 'PLS')
-  const query = querystring.stringify(parametros)
-  const itens = get(context, 'params.itens', 15)
+    const urlProposicoes = query => `http://legis.senado.leg.br/dadosabertos/materia/atualizadas?${query}`
+    const urlProposicoesDetalhes = id => `http://legis.senado.leg.br/dadosabertos/materia/${id}`
 
-  const proposicoes = take(await httpGetManyMaterias(urlProposicoes(query)), itens)
-  const proposicoesDetalhadas = await Promise.all(
-    proposicoes
-      .map(p => p.IdentificacaoMateria.CodigoMateria)
-      .map(urlProposicoesDetalhes)
-      .map(httpGetOneMateria)
-  )
+    const httpGetManyMaterias = async (url) => {
+        const res = await axios.get(url)
+        return get(res, 'data.ListaMateriasAtualizadas.Materias.Materia', [])
+    }
 
-  return proposicoesDetalhadas.map((proposicao) => ({
-    id: proposicao.IdentificacaoMateria.CodigoMateria,
-    siglaTipo: proposicao.IdentificacaoMateria.SiglaSubtipoMateria,
-    dataApresentacao: new Date(proposicao.DadosBasicosMateria.DataApresentacao),
-    situacao: proposicao.SituacaoAtual.Autuacoes.Autuacao.Situacao.DescricaoSituacao,
-    numero: proposicao.IdentificacaoMateria.NumeroMateria,
-    ano: proposicao.IdentificacaoMateria.AnoMateria,
-    ementa: proposicao.DadosBasicosMateria.EmentaMateria,
-    autor: getAutorAleatorio(proposicao),
-    casa: CASA
-  }))
+    const httpGetOneMateria = async (url) => {
+        const res = await axios.get(url)
+        return get(res, 'data.DetalheMateria.Materia', {})
+    }
+
+    const getAutorAleatorio = (proposicao) => {
+        const autores = get(proposicao, 'Autoria.Autor', [])
+        const autorAleatorio = autores[random(0, autores.length - 1)] || {}
+        const getProp = partial(get, autorAleatorio)
+        return {
+            id: getProp('IdentificacaoParlamentar.CodigoParlamentar'),
+            nome: getProp('NomeAutor'),
+            foto: getProp('IdentificacaoParlamentar.UrlFotoParlamentar')
+        }
+    }
+
+    const parametros = {...context.params}
+    parametros.sigla = get(context, 'params.siglaTipo', 'PLS')
+    const query = querystring.stringify(parametros)
+    const itens = get(context, 'params.itens', 15)
+
+    const proposicoes = take(await httpGetManyMaterias(urlProposicoes(query)), itens)
+    const proposicoesDetalhadas = await Promise.all(
+        proposicoes
+            .map(p => p.IdentificacaoMateria.CodigoMateria)
+            .map(urlProposicoesDetalhes)
+            .map(httpGetOneMateria)
+    )
+
+    return proposicoesDetalhadas.map((proposicao) => ({
+        id: proposicao.IdentificacaoMateria.CodigoMateria,
+        siglaTipo: proposicao.IdentificacaoMateria.SiglaSubtipoMateria,
+        dataApresentacao: new Date(proposicao.DadosBasicosMateria.DataApresentacao),
+        situacao: proposicao.SituacaoAtual.Autuacoes.Autuacao.Situacao.DescricaoSituacao,
+        numero: proposicao.IdentificacaoMateria.NumeroMateria,
+        ano: proposicao.IdentificacaoMateria.AnoMateria,
+        ementa: proposicao.DadosBasicosMateria.EmentaMateria,
+        autor: getAutorAleatorio(proposicao),
+        casa: CASA
+    }))
 }
